5d9d8c38cb8229995801611b7952a8a7196a5c1f
dheeru_bhai <167254577+TechnicalDheeru@users.noreply.github.com>
2025-07-10 01:58:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close'),
+      closeNavLinks = document.getElementById('close-nav-links')
+
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+/* Nav Home */ 
+if(closeNavLinks){
+   closeNavLinks.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
